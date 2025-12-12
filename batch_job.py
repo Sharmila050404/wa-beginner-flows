740d60445d99d5850c231133238d423042139cd1
Sharmila050404 <N.Sharmila@ibm.com>
2025-12-12 18:05:15 +0530

    Update batch_job.py

--- batch_job.py
+++ batch_job.py
@@ -14,6 +14,7 @@ def load(data):
 
 @flow
 def etl_flow(job_name: str = "Default Job"):
+    print(f"Job Name: {job_name}")
     raw = extract()
     processed = transform(raw)
     load(f"{job_name}: {processed}")
